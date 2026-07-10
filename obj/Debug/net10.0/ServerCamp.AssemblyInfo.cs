008c682a6b6fa42c627c9aaba044bf2b74a27772
Ismail Maowla Upal <ismailupal1234@gmail.com>
2026-07-10 23:56:40 +0600

    10 jul

--- obj/Debug/net10.0/ServerCamp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ServerCamp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCamp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a5bb8c3c69ff6a7b3e64e2b7274afa912f5245")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCamp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCamp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
