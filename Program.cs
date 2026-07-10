80a5bb8c3c69ff6a7b3e64e2b7274afa912f5245
Ismail Maowla Upal <ismailupal1234@gmail.com>
2026-07-10 23:51:32 +0600

    10jul

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Dynamic;
+
+public class House
+{
+   public List Rooms { get; } = new();
+   public House()
+   {
+      Rooms.Add(new Room("Living Room"));
+      Rooms.Add(new Room("Kitchen"));
+   }
+   public class Room
+   {
+      public string Name { get; }
+      public Room(string name)
+      {
+         Name = name;
+      }
+   }        
+}
+
